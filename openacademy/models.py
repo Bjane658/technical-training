386db8f9f9302a956c5be7153c2c32d1dd494102
Bjane658 <bjornbjorn@hotmail.de>
2018-10-01 12:50:04 +0000

    Contrain added to the openacademy

--- openacademy/models.py
+++ openacademy/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class Courses(models.Model):
     _name = 'openacademy.course'
@@ -16,4 +18,9 @@ class Sessions(models.Model):
     user_id = fields.Many2one('res.users', string="Instructor")
     start_date = fields.Date()
     seats = fields.Integer('Room Capacity')
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    attendee_ids = fields.Many2many('res.partner', string="Attendees", )
+    
+    @api.constrains('attendee_ids')
+    def _checkAttendeeCount(self):
+        if len(self.attendee_ids) > self.seats:
+            raise ValidationError("There are too manny Attendees")
